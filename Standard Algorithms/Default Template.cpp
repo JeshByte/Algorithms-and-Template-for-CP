3d056fbd49cd751387686d5693b1917c526d40d6
JeshByte <159941901+JeshByte@users.noreply.github.com>
2024-10-24 19:53:48 +0530

    Create Default Template.cpp

---
+++ Standard Algorithms/Default Template.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+using ll=long long;
+#define Ceil(a,b) ((a+b-1)/b)
+void solve(ll t){
+    
+}
+int main(){
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+  ll t=1;
+  cin>>t;
+  for(int i=1;i<=t;i++){
+  solve(i);
+  }
+  return 0;
+} 
