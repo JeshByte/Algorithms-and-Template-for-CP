9802b75e49f22ec37b964312c64d2ac7bf65b733
Prajesh Biswas <159941901+JeshByte@users.noreply.github.com>
2025-01-11 23:30:42 +0530

    Update Floyd's Cycle Detection Algorithm.cpp

--- Linked List Algorithms/Floyd's Cycle Detection Algorithm.cpp
+++ Linked List Algorithms/Floyd's Cycle Detection Algorithm.cpp
@@ -1,21 +1,27 @@
-bool isCircular(Node* head){
-    if(head==NULL){
-        return true;
-    }
-    int val=head->data;
+Node *removeLoop(Node *head)
+{
     Node* fast=head;
     Node* slow=head;
+    bool isCycle=false;
     while(fast!=NULL && fast->next!=NULL){
         slow=slow->next;
         fast=fast->next->next;
         if(slow==fast){
-            if(slow->data==val){
-                return true;
-            }
-            else{
-                return false;
-            }
+            isCycle=true;
+            break;
         }
     }
-    return false;
+    if(isCycle){
+        fast=head;
+        while(fast!=slow){
+            fast=fast->next;
+            slow=slow->next;
+        }
+        Node* temp=fast;
+        while(fast->next!=temp){
+            fast=fast->next;
+        }
+        fast->next=NULL;
+    }
+    return head;
 }
