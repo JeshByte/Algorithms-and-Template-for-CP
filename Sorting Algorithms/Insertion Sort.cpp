2b61d1902ff3b92a319007d7a02c8401716b2c95
Prajesh Biswas <159941901+JeshByte@users.noreply.github.com>
2025-01-12 19:34:53 +0530

    Update Insertion Sort.cpp

--- Sorting Algorithms/Insertion Sort.cpp
+++ Sorting Algorithms/Insertion Sort.cpp
@@ -1,1 +1,13 @@
-efjnf4j
+#include <bits/stdc++.h> 
+using namespace std;
+void insertionSort(int n, vector<int> &arr){
+    for(int i=1;i<n;i++){
+        int key=arr[i];
+        int j=i-1;
+        while(j>=0 && key<arr[j]){
+            arr[j+1]=arr[j];
+            j--;
+        }
+        arr[j+1]=key;
+    }
+}
