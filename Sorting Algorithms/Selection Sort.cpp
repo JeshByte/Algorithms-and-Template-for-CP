753ad402d753d9e47f068ba0f6d2d4581ac59362
Prajesh Biswas <159941901+JeshByte@users.noreply.github.com>
2025-01-18 19:54:09 +0530

    Update Selection Sort.cpp

--- Sorting Algorithms/Selection Sort.cpp
+++ Sorting Algorithms/Selection Sort.cpp
@@ -1,1 +1,16 @@
-3j4t34
+#include <bits/stdc++.h> 
+using ll=long long;
+using namespace std;
+void selectionSort(vector<int>& arr, int n)
+{   
+    for(int i=0;i<n;i++){
+        ll mi=1001,ind;
+        for(int j=i;j<n;j++){
+            if(arr[j]<mi){
+                mi=arr[j];
+                ind=j;
+            }
+        }
+        swap(arr[i],arr[ind]);
+    }
+}
