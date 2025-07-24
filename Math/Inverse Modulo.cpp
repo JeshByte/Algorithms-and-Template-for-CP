7498d6917272d609a8913fc0d00b8895480d256a
Prajesh Biswas <159941901+JeshByte@users.noreply.github.com>
2025-07-24 22:27:36 +0530

    Create Inverse Modulo.cpp

---
+++ Math/Inverse Modulo.cpp
@@ -0,0 +1,16 @@
+long long modExpo(long long base, long long exp, long long mod) {
+    long long result = 1;
+    while (exp > 0) {
+        if (exp % 2 == 1) {
+            result = (result * base) % mod;
+        }
+ 
+        exp = exp >> 1;
+ 
+        base = (base * base) % mod;
+    }
+    return result;
+}
+ll findInverse(ll n,ll MOD){
+    return modExpo(n,MOD-2,MOD);
+}
