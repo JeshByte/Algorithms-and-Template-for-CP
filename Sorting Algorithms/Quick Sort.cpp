822fd1432541583dba24a653a31fc1e62b5a5768
Prajesh Biswas <159941901+JeshByte@users.noreply.github.com>
2025-08-04 15:55:36 +0530

    Update Quick Sort.cpp

--- Sorting Algorithms/Quick Sort.cpp
+++ Sorting Algorithms/Quick Sort.cpp
@@ -1,1 +1,40 @@
-3orh3o
+#include <bits/stdc++.h>
+int partition(vector<int>&arr,int s,int e){
+    int pivot=arr[s];
+    int co=0;
+    for(int i=s;i<=e;i++){
+        if(arr[i]<pivot){
+            co++;
+        }
+    }
+    int ind=s+co;
+    swap(arr[s],arr[ind]);
+    int i=s,j=e;
+    while(i<ind && j>ind){
+        if(arr[i]<pivot){
+            i++;
+            continue;
+        }
+        if(arr[j]>=pivot){
+            j--;
+            continue;
+        }
+        swap(arr[i],arr[j]);
+        i++;
+        j--;
+    }
+    return ind;
+}
+void sort(vector<int>&arr,int s,int e){
+    if(s>=e){
+        return;
+    }
+    int ind=partition(arr,s,e);
+    sort(arr,s,ind-1);
+    sort(arr,ind+1,e);
+}
+vector<int> quickSort(vector<int> arr)
+{
+    sort(arr,0,arr.size()-1);
+    return arr;
+}
